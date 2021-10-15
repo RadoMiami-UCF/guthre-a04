24cd2372eba4e0ec1cf52c49b5880ffc6f818788
RadoMiami <kguthre@gmail.com>
2021-10-15 13:20:24 -0400

    Added extra function to pseudocode for exercise43

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,11 +1,20 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution43 {
+    private static final Scanner in = new Scanner(System.in, "UTF_8");
+
     public static void main(String[] args) {
         //First, ask for the site name.
         //Then, ask for the author.
         //Then, ask if they want a folder for JavaScript and CSS.
         //Then, pass these parameters into a new WebsiteBuilder object named websiteBuilder.
         //Finally, use websiteBuilder.createDirectories to create the directories.
+    }
+
+    private String askThenReturnString(String askString) {
+        System.out.print(askString);
+        return in.nextLine();
     }
 }
